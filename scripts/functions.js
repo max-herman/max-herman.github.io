5a9fe250e0b42dcbce152f01d73ca67ab9159340
Maxfield Herman <leawoodmax@yahoo.com>
2020-01-22 20:20:06 -0500

    removed features found on desktop app, edited cells for mobile

--- scripts/functions.js
+++ scripts/functions.js
@@ -9,11 +9,8 @@ var currCell_y = 0;
 $(document).ready(function(e) {
 
     $(".blank").click(function(e) {
-        var oTable = document.getElementsByClassName('grid')[0].getElementsByTagName("tbody")[0];
-        oTable.rows[currCell_y].cells[currCell_x].style.backgroundColor = "white";
         currCell_x = e.target.cellIndex;
         currCell_y = e.target.parentNode.id;
-        oTable.rows[currCell_y].cells[currCell_x].style.backgroundColor = "lightgrey";
     });
 
     $(".gwt-MenuItem").click(function(e) {
@@ -22,7 +19,6 @@ $(document).ready(function(e) {
         } else if(e.target.innerHTML == "Solve") {
             var board = build_board();
             console.log(solve_board(board));
-            console.log(board);
             inject_board(board);
         } else if(e.target.innerHTML == "Help") {
             alert("How to Use:\nClick a cell and enter a digit.\nUse arrow keys to navigate as well.\nIf you enter a number in the wrong cell, replace with a 0 to empty it.\nOnce you have all numbers entered, press solve to solve the puzzle.\nPress the Clear button to empty the board.");
@@ -32,11 +28,10 @@ $(document).ready(function(e) {
 
 document.addEventListener('keydown', function(event) {
     var oTable = document.getElementsByClassName('grid')[0].getElementsByTagName("tbody")[0];
-    oTable.rows[currCell_y].cells[currCell_x].style.backgroundColor = "white";
     var val = String.fromCharCode(event.keyCode);
     if(currCell_x >= 0 && currCell_x <= 8 && currCell_y >= 0 && currCell_y <= 8) {
         if(!isNaN(parseInt(val))) {
-            oTable.rows[currCell_y].cells[currCell_x].innerHTML = parseInt(val);
+            oTable.rows[currCell_y].cells[currCell_x].getElementsByTagName("textarea")[0].value = parseInt(val);
         } else if(event.keyCode == 37 && currCell_x > 0) {
             currCell_x--;
         } else if(event.keyCode == 38 && currCell_y > 0) {
@@ -47,7 +42,6 @@ document.addEventListener('keydown', function(event) {
             currCell_y++;
         }
     }
-    oTable.rows[currCell_y].cells[currCell_x].style.backgroundColor = "lightgrey";
 });
 
 function build_board() {
@@ -64,8 +58,8 @@ function build_board() {
 
         for(var j = 0; j < cellLength; j++){
 
-            var cellVal = oCells.item(j).innerHTML;
-            if(cellVal !== "&nbsp;") {
+            var cellVal = oCells.item(j).getElementsByTagName("textarea")[0].value;
+            if(cellVal !== "") {
                 row.push(parseInt(cellVal));
             } else {
                 row.push(0);
@@ -88,7 +82,7 @@ function clear_board() {
 
         for(var j = 0; j < cellLength; j++){
 
-            oCells.item(j).innerHTML = "&nbsp;";
+            oCells.item(j).getElementsByTagName("textarea")[0].value = "";
         }
     }
 }
@@ -104,7 +98,7 @@ function inject_board(board) {
 
         for(var j = 0; j < cellLength; j++){
 
-            oCells.item(j).innerHTML = board[i][j];
+            oCells.item(j).getElementsByTagName("textarea")[0].value = board[i][j];
         }
     }
 }
